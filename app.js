be10c3cc08e95830ad5e393037ea9962c468bfc2
Lizzy Britton <lizzy@curalate.com>
2014-12-08 17:35:50 -0500

    Change port

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});  
   response.write("Hello from the Node.js server!");  
   response.end();
-}).listen(8080);
+}).listen(8081);
 console.log('Server is listening to http://localhost/ on port 8080…');
